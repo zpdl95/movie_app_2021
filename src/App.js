ff780771ef44e1e6948a5bfca7664bcf0c24c948
zpdl95 <59528534+zpdl95@users.noreply.github.com>
2021-08-11 22:33:41 +0900

    #3.3 Planning the Movie Component

--- src/App.js
+++ src/App.js
@@ -2,41 +2,17 @@ import React from "react";
 
 class App extends React.Component {
   state = {
-    count: 0,
+    isLoading: true,
+    movies: [],
   };
-  add = () => {
-    this.setState((current) => ({ count: current.count + 1 }));
-  };
-  minus = () => {
-    this.setState((current /*현재 state값을 표기*/) => ({
-      count: current.count - 1,
-    }));
-  }; /* setState는 state를 바꾸고 render()함수를 실행시킴 */
-
   componentDidMount() {
-    console.log(
-      "Component rendered"
-    ); /* 새로고침등 컴포넌트가 처음 불러와질때 */
-  }
-  componentDidUpdate() {
-    console.log(
-      "I just updated"
-    ); /* 동적함수의 작동으로 페이지에 표시된 수치가 바뀔때 */
-  }
-  componentWillUnmount() {
-    console.log(
-      "Goodbye, cruel world"
-    ); /* 컴포넌트의 내용이 바뀌어 새로 표시해야할때, 컴포넌트가 종료될때 */
+    setTimeout(() => {
+      this.setState({ isLoading: false });
+    }, 6000);
   }
   render() {
-    console.log("I'm rendering");
-    return (
-      <div>
-        <h1>The number is: {this.state.count}</h1>
-        <button onClick={this.add}>Add</button>
-        <button onClick={this.minus}>Minus</button>
-      </div>
-    );
+    const { isLoading } = this.state;
+    return <div>{isLoading ? "Loading..." : "We are ready"}</div>;
   }
 }
 
